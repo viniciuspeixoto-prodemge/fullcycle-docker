c058ad913d8392855f41ad95c1ee6a2a5de7c5f8
Your Name <you@example.com>
2023-08-07 11:12:22 -0300

    inicial

--- NginxComNode.js/index.js
+++ NginxComNode.js/index.js
@@ -10,22 +10,18 @@ const config = {
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
-//const sql = `INSERT INTO people(name) values('Vinicius')`
-//connection.query(sql)
-//connection.end()
+const sql = `INSERT INTO people(name) values('Vinicius')`
+connection.query(sql)
+connection.end()
+
+//connection.query("SELECT * FROM people", function (res) {
+//  res.send(result)
+//});
 
 app.get('/', (req,res) => {
     res.send('<h1>Full Cycle Rocks!</h1>')
 })
 
-const sql2 = `SELECT * FROM people`
-connection.query(sql2, function (err, result, fields) {
-  Object.keys(result).forEach(function(key) {
-      var row = result[key];
-      res.send(row.name)
-    });
-})
-connection.end()
 
 app.listen(port, ()=> {
     console.log('Rodando na porta ' + port)
